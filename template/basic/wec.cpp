e5525f017a942ea940f94c81825371ee3cd9cd3f
Karthikeya Kona <konakarthik12@gmail.com>
2024-05-06 17:43:49 -0400

    redesign vec to offset index

--- template/basic/wec.cpp
+++ template/basic/wec.cpp
@@ -1,66 +1,18 @@
 #pragma once
 #include "bool.cpp"
+#include "con.cpp"
+#include "../wrapper.h"
 
 template <typename V>
 struct wec : public Con<vector<typename NotBool<V>::type>> {
   using T = NotBool<V>::type;
-  using C = Con<vector<T>>;
+  using Con<vector<typename NotBool<V>::type>>::Con;
 
-  wec() : C(1) {
-  }
-  explicit wec(int size) : C(size + 1) {
-  }
-  explicit wec(int size, T init) : C(size + 1, init) {
+  T& operator[](size_t index) {
+    return this->v[index - 1];
   }
 
-  wec(initializer_list<V> initList) : C(initList.size() + 1) {
-    auto it = this->v.begin() + 1;
-    for (const auto& value: initList) {
-      *it++ = value;
-    }
-  }
-
-  int size() const {
-    return this->v.size() - 1;
-  }
-  bool empty() const {
-    return this->v.size() == 1;
-  }
-  void reserve(int size) {
-    this->v.reserve(size + 1);
-  }
-
-  void resize(int size) {
-    this->v.resize(size + 1);
-  }
-  void assign(int size, int x = T()) {
-    this->v.assign(size + 1, x);
-  }
-  auto begin() {
-    return this->v.begin() + 1;
-  }
-
-  auto begin() const {
-    return this->v.begin() + 1;
-  }
-
-  T& front() {
-    return this->v[1];
-  }
-
-
-
-  void clear() {
-    this->v.resize(1);
-  }
-
-  wec operator+(const wec& other) const {
-    wec result = *this;
-    result.insert(result.end(), other.begin(), other.end());
-    return result;
-  }
-
-  explicit operator bool() const {
-    return this->v.size() > 1;
+  const T& operator[](size_t index) const {
+    return this->v[index - 1];
   }
 };
