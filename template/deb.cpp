c168c9d295426d2153537fbfcd90ee7cae1180c5
Karthikeya Kona <konakarthik12@gmail.com>
2024-06-06 20:18:10 -0400

    debug cleanup

--- template/deb.cpp
+++ template/deb.cpp
@@ -1,75 +1,75 @@
 #pragma once
 #include "base.cpp"
+ostream& dout = cerr;
 
 template <typename Container>
   requires(HasValueType1D<Container> && !Printable<Container>)
-
-void debug(ostream& os, Container v);
+void debug(Container v);
 template <typename T, typename V>
-void debug(ostream& out, pr<T, V> const& p);
+void debug(pr<T, V> const& p);
 
 template <const unsigned long N>
-void debug(ostream& out, typename bitset<N>::reference v) {
-  out << (bool) v;
+void debug(typename bitset<N>::reference v) {
+  dout << (bool) v;
 }
 
 template <const unsigned long N>
-void debug(ostream& out, bitset<N> b) {
-  out << b;
+void debug(bitset<N> b) {
+  dout << b;
 }
 template <typename T>
   requires Printable<T>
-void debug(ostream& out, T x) {
-  out << x;
+void debug(T x) {
+  dout << x;
 }
 
 template <typename T, typename V>
-void debug(ostream& out, pair<T, V> const& p) {
-  out << '(';
-  debug(out, p.first);
-  out << ", ";
-  debug(out, p.second);
-  out << ')';
+void debug(pair<T, V> const& p) {
+  dout << '(';
+  debug(dout, p.first);
+  dout << ", ";
+  debug(dout, p.second);
+  dout << ')';
 }
 
 template <typename T, typename V>
-void debug(ostream& out, pr<T, V> const& p) {
-  out << '(';
-  debug(out, p.f);
-  out << ", ";
-  debug(out, p.s);
-  out << ')';
+void debug(pr<T, V> const& p) {
+  dout << '(';
+  debug(dout, p.f);
+  dout << ", ";
+  debug(dout, p.s);
+  dout << ')';
 }
 
 template <size_t Index = 0, typename... Ts>
-void tuple_debug(ostream& out, const tuple<Ts...>& t) {
+void tuple_debug(const tuple<Ts...>& t) {
   if constexpr (Index < sizeof...(Ts)) {
-    if (Index != 0) out << ", ";
-    debug(out, get<Index>(t));
-    tuple_debug<Index + 1>(out, t);
+    if (Index != 0) dout << ", ";
+    debug(dout, get<Index>(t));
+    tuple_debug<Index + 1>(dout, t);
   }
 }
 
 template <typename... Ts>
-void debug(ostream& out, const tuple<Ts...>& t) {
-  out << "(";
-  tuple_debug(out, t);
-  out << ")";
+void debug(const tuple<Ts...>& t) {
+  dout << "(";
+  tuple_debug(dout, t);
+  dout << ")";
 }
 
 template <typename T, typename V, typename U>
-void debug(ostream& out, priority_queue<T, V, U> p) {
+void debug(priority_queue<T, V, U> p) {
   vector<T> v;
   while (!p.empty()) {
     v.pb(p.top());
     p.pop();
   }
-  debug(out, v);
+  debug(dout, v);
 }
 
 template <typename Container>
   requires(HasValueType1D<Container> && !Printable<Container>)
-void debug(ostream& os, Container v) {
+void debug(Container v) {
   os << '[';
   for (auto it = v.begin(); it != v.end(); ++it) {
     if (it != v.begin()) {
@@ -82,26 +82,26 @@ void debug(ostream& os, Container v) {
 
 template <typename Container>
   requires HasValueType2D<Container>
-void debug(ostream& out, Container v) {
-  out << "\n[";
+void debug(Container v) {
+  dout << "\n[";
   for (auto it = v.begin(); it != v.end(); ++it) {
     if (it != v.begin()) {
-      out << ",\n ";
+      dout << ",\n ";
     }
-    debug(out, *it);
+    debug(dout, *it);
   }
-  out << ']';
+  dout << ']';
 }
 
 void debug_all(ostream&) {
 }
 template <typename Arg, typename... Args>
-void debug_all(ostream& out, Arg&& arg, Args&&... args) {
-  debug(out, arg);
+void debug_all(Arg&& arg, Args&&... args) {
+  debug(dout, arg);
   if (sizeof...(args) > 0) {
-    out << ", ";
+    dout << ", ";
   }
-  debug_all(out, args...);
+  debug_all(dout, args...);
 }
 #ifdef __JETBRAINS_IDE__
 #define LOCAL
@@ -111,24 +111,24 @@ void debug_all(ostream& out, Arg&& arg, Args&&... args) {
 #define deb(...) logger(cerr, __LINE__, false, #__VA_ARGS__, ##__VA_ARGS__);
 #define deb_n(...) logger(cerr, __LINE__, true, #__VA_ARGS__, ##__VA_ARGS__);
 template <typename... Args>
-void logger(ostream& out, int line, bool nest, string vars, Args&&... values) {
+void logger(int line, bool nest, string vars, Args&&... values) {
   if (vars.size() == 0) {
-    out << "\n";
+    dout << "\n";
     return;
   }
   if (nest) {
     void* callstack[20];
     int frames = backtrace(callstack, 20);
     for (int i = 4; i < frames; i++) {
-      out << "  ";
+      dout << "  ";
     }
   }
-  out << "\033[30;1m" << line << ": ";
-  out << vars << " = ";
-  debug_all(out, values...);
-  //  logger(out, line, nest, vars, forward<Args>(values)...)
-  //  (..., (out << delim << values, delim = ", "));
-  out << "\033[0m\n";
+  dout << "\033[30;1m" << line << ": ";
+  dout << vars << " = ";
+  debug_all(dout, values...);
+  //  logger(dout, line, nest, vars, forward<Args>(values)...)
+  //  (..., (dout << delim << values, delim = ", "));
+  dout << "\033[0m\n";
 }
 #define dssert(...) assert(__VA_ARGS__);
 __attribute__((unused)) const bool prod = false;
